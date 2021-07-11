5ad5c4447cb4cb7c5ec073779ca29d167daac5a2
Sebastian F <sebpfave2@gmail.com>
2021-07-10 22:50:45 -0400

    Seetup server typeDef and resolver starter code

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { Book, User } = require('../models');
+
+const resolvers = {
+  Query: {},
+
+  Mutation: {},
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+
+}
+
+type Book {
+
+}
+
+type Query {
+
+}
+
+type Mutation {
+
+}`;
+
+module.exports = typeDefs;
