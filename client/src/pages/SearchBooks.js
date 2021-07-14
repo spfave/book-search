3643ef03135b643b9bc83be6b03b47a89d3b653e
Sebastian F <sebpfave2@gmail.com>
2021-07-14 17:16:32 -0400

    Refactor save bookId to local storage on save

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -29,8 +29,9 @@ const SearchBooks = () => {
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
-    return () => saveBookIds(savedBookIds);
-  });
+    saveBookIds(savedBookIds);
+    // return () => saveBookIds(savedBookIds);
+  }, [savedBookIds]);
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
