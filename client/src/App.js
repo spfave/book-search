be076c5904329ddb45200bc056fc1eaa2a20ad38
Sebastian F <sebpfave2@gmail.com>
2021-07-26 07:11:24 -0400

    Config heroku routing

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,7 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path='/' component={SearchBooks} />
-            <Route path='/saved' component={SavedBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Switch>
         </>
